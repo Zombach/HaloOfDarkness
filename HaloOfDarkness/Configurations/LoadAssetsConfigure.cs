47194b6e3a28ec7450a8555270d4a31d0cc25c3b
ZLoo <Kozlov.s.v.1992@gmail.com>
2024-02-23 17:11:15 +0300

    Подключил файлы конфигурации приложения

---
+++ HaloOfDarkness/Configurations/LoadAssetsConfigure.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace HaloOfDarkness.Configurations;
+
+public static class LoadAssetsConfigure
+{
+    public static Stream LoadAsset(string fileName)
+    {
+        var stream = Task.Run(async () => await FileSystem.OpenAppPackageFileAsync(fileName)).Result;
+        ArgumentNullException.ThrowIfNull(stream, $"Не удалось получить файл {fileName}");
+        return stream;
+    }
+
+    public static void AddJsonConfigure(this ConfigurationManager configurationManager, string fileName)
+    {
+        using var stream = LoadAsset(fileName);
+        var config = new ConfigurationBuilder().AddJsonStream(stream).Build();
+        configurationManager.AddConfiguration(config);
+    }
+}
--- HaloOfDarkness/MauiProgram.cs
+++ HaloOfDarkness/MauiProgram.cs
@@ -11,6 +11,14 @@ public static class MauiProgram
         try
         {
             var builder = MauiApp.CreateBuilder();
+            builder.Configuration.AddJsonConfigure("appsettings.json");
+#if DEBUG
+            builder.Configuration.AddJsonConfigure("appsettings.development.json");
+#else
+            builder.Configuration.AddJsonConfigure("appsettings.production.json");
+#endif
+
+
             builder
                 .UseMauiApp<App>()
                 .ConfigureFonts(fonts =>
