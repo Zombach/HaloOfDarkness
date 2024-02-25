90e73f15a38f841d2041cc0b011685b10fa5ee6d
Eugene <kulinich.eugene.2412@gmail.com>
2024-02-26 00:17:33 +0600

    add logger

---
+++ Infrastructure/Extension/LoggerExtension.cs
@@ -0,0 +1,15 @@
+﻿using Serilog;
+
+namespace Infrastructure.Extension;
+
+public static class LoggerExtension
+{
+    public static void AddLogger()
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .WriteTo.File("logs/myapp.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+    }
+}
